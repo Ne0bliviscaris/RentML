574ba52ca4ed133d7bdda1a1203a86ae8a0a1e3f
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-02-08 17:20:03 +0100

    introduced session_state to prevent recalculating images

--- main.py
+++ main.py
@@ -11,25 +11,33 @@ from modules.view_plot import show_altair_chart
 st.set_page_config(page_title="Car Mileage Analysis", page_icon="🚗")
 
 
-def left_column():
+def image_processing():
     """Left column handles image upload and preview."""
-    img = uploader()
-    if img:
-        st.image(img, use_column_width=True)
+    st.session_state.image = uploader()
+    if st.session_state.image:
+        st.image(st.session_state.image, use_column_width=True)
         # preprocessed_img = preprocess(img)  #Potential image preprocessing here
-        data = extract_data(img)
-        return data
+        if not st.session_state.image_processed:
+            st.session_state.extracted_data = extract_data(st.session_state.image)
+            st.session_state.image_processed = True
 
 
 def main():
+    if "image" not in st.session_state:
+        st.session_state.image = None
+    if "image_processed" not in st.session_state:
+        st.session_state.image_processed = False
+    if "extracted_data" not in st.session_state:
+        st.session_state.extracted_data = None
+
     left_col, right_col = st.columns(2)
 
     with left_col:
-        data = left_column()
+        image_processing()
 
     with right_col:
-        if data is not None:
-            confirmation_form(data)
+        if st.session_state.extracted_data is not None:
+            confirmation_form(st.session_state.extracted_data)
 
     # show_altair_chart()
 
