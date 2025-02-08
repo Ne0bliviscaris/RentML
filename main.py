b872000bf6fce6e049cee1529d7190464f312295
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-02-08 17:06:42 +0100

    refactor

--- main.py
+++ main.py
@@ -4,73 +4,34 @@ import subprocess
 
 import streamlit as st
 
-import modules.detect_car as detect
-import modules.streamlit_functions as sf
-import modules.view_plot as alt_plot
+from modules.data_processing import extract_data
+from modules.streamlit_functions import confirmation_form, uploader
+from modules.view_plot import show_altair_chart
 
-# st.set_page_config(page_title="Car Mileage Analysis", page_icon="🚗", layout="wide")
-
-
-def uploader():
-    # if st.button("Camera"):
-    #     return st.camera_input("Take a photo")
-    # if st.button("Upload image"):
-    return st.file_uploader("Select image", type=["jpg", "png", "jpeg", "gif"])
+st.set_page_config(page_title="Car Mileage Analysis", page_icon="🚗")
 
 
-def show_chart():
-    chart = alt_plot.prepare_plot()
-    if chart:
-        st.altair_chart(chart)
-
-
-def extract_data(image):
-    if image is not None:
-        mileage = sf.extract_mileage(image)
-        car_type = detect.identify_car(image)
-        return [mileage, car_type]
-
-
-def upload_form():
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.write("Upload image")
-        uploaded_img = uploader()
-        data = extract_data(uploaded_img)
-
-    with col2:
-        if data is not None:
-            st.write(data)
-            confirmation_form(data)
-            if st.button("Confirm"):
-                # sf.extract_and_append_data(uploaded_img.name, data[0], data[1])
-                st.write(data[0], data[1])
-
-
-def confirmation_form(data):
-    pass
-
-
-# # Deprecated
-def process_file(image):
-    path = f"data\\main-dataset\\{image.name}"
-    numpy_img = sf.load_image(image)
-    mileage = sf.extract_mileage(numpy_img)
-    car_type = detect.identify_car(image)
-    sf.extract_and_append_data(path, mileage, car_type)
-    sf.load_and_display_data(path)
+def left_column():
+    """Left column handles image upload and preview."""
+    img = uploader()
+    if img:
+        st.image(img, use_column_width=True)
+        # preprocessed_img = preprocess(img)  #Potential image preprocessing here
+        data = extract_data(img)
+        return data
 
 
 def main():
-    # upload_form()
+    left_col, right_col = st.columns(2)
 
-    uploaded_img = uploader()
+    with left_col:
+        data = left_column()
 
-    if uploaded_img is not None:
-        process_file(uploaded_img)
+    with right_col:
+        if data is not None:
+            confirmation_form(data)
 
-    show_chart()
+    # show_altair_chart()
 
 
 if __name__ == "__main__":
--- modules/streamlit_functions.py
+++ modules/streamlit_functions.py
@@ -1,83 +1,76 @@
-# v1.01 - main streamlit application functions
-import json
-import os
+from datetime import datetime
 
-import numpy as np
 import pandas as pd
 import streamlit as st
-from PIL import Image, ImageOps
 
-import modules.date_extract as date_extract
-import modules.export as export
-import modules.ocr as ocr
-import modules.preprocessing as pp  # Currently not used - needed for preprocessing
+import modules.data_processing as sf
+from modules.handover_protocol import print_handover_protocol
 
 
-# File upload and image display via Streamlit
-def load_image(uploaded_file):
-    """Load an image from a file, and return it as a NumPy array."""
-    img = Image.open(uploaded_file)
-    img = ImageOps.exif_transpose(img)
-    st.image(img, caption="Wybrane zdjęcie", use_column_width=True)
-    return np.array(img)
+def uploader():
+    # if st.button("Camera"):
+    #     return st.camera_input("Take a photo")
+    # if st.button("Upload image"):
+    return st.file_uploader("Dodaj zdjęcie", type=["jpg", "png", "jpeg", "gif"])
 
 
-# OCR and preprocessing - currently preprocessing is skipped
-def extract_mileage(img):
-    """Perform OCR on image."""
-    st.write("OCR Mileage:")
-    mileage = ocr.mileage_ocr(img)
-    if mileage is None:
-        st.write("Mileage not recognized")
-    else:
-        st.write(mileage[0])
-    return mileage[0] if mileage is not None else None
+def confirmation_form(data):
+    """Display editable form with pre-filled mileage and car type."""
+    mileage, car, date, time = data
+
+    with st.form("Potwierdzenie danych", clear_on_submit=True, border=0):
+        # Edytowalny przebieg
+        mileage = editable_mileage_field(mileage)
+        car = editable_car_selector(car)
+        date = editable_date_field(date)
+        time = editable_time_field(time)
+        notes = editable_notes_field()
+
+        col1, col2 = st.columns(2)
+        with col1:
+            submitted = st.form_submit_button("Zapisz")
+            if submitted:
+                success = sf.append_to_json(
+                    file_path=None, mileage=mileage, car_type=car, date=date, time=time, note=notes
+                )
+                if success:
+                    st.success("Zapisano dane")
+                    st.session_state.form_submitted = True
+                else:
+                    st.warning("Dane istnieją już w bazie. Zaktualizowano notatkę.")
+
+        with col2:
+            handover = st.form_submit_button("Print Handover Protocol")
+            if handover:
+                print_handover_protocol(mileage=mileage, car_type=car, date=date, time=time, note=notes)
 
 
-# Extract data from file and append to JSON file
-def extract_and_append_data(path, mileage, car_type, note=None):
-    """Extract data from file and append it to JSON file."""
-    # Check if JSON file is empty
-    if os.path.getsize("data/result/mileage.json") == 0:
-        rep_check = []
-    else:
-        # Read the JSON file
-        with open("data/result/mileage.json", "r") as f:
-            rep_check = json.load(f)
+def editable_mileage_field(mileage):
+    return st.number_input("Przebieg", min_value=0, max_value=999999, value=int(mileage) if mileage else 0, step=1)
 
-    # Check if the file is already in the JSON file
-    for entry in rep_check:
-        if entry["Filename"] == path:
-            return f"File {path} already exists."
 
-    # Extract date and time from filename
-    date, time = date_extract.extract_time_from_filename(path)
-    export.append_to_json(
-        "data/result/mileage.json",
-        [
-            {
-                "Filename": path,
-                "Date": date,
-                "Time": time,
-                "Mileage": str(mileage),
-                "Type": car_type,
-                "Notes": note,
-            }
-        ],
+def editable_date_field(date_str):
+    try:
+        date = datetime.strptime(date_str, "%Y-%m-%d").date()
+    except (ValueError, TypeError):
+        date = datetime.now().date()
+    return st.date_input("Data", value=date)
+
+
+def editable_time_field(time_str):
+    """Display editable time input field."""
+    try:
+        time = datetime.strptime(time_str, "%H:%M:%S").time()
+    except:
+        time = datetime.now().time()
+    return st.time_input("Godzina", value=time)
+
+
+def editable_notes_field():
+    return st.text_area("Notatki", value="")
+
+
+def editable_car_selector(car):
+    return st.selectbox(
+        "Select car", options=["Osobowy", "Dostawczy L3H2", "Dostawczy L4H2"], index=1 if car == "Osobowy" else 0
     )
-
-
-# Load and display data from JSON file in Streamlit as a DataFrame
-def load_and_display_data(path: str) -> None:
-    """
-    Load data from a JSON file, display the record associated with a specific path in Streamlit,
-    Optionally display all records in the JSON file in Streamlit as a DataFrame. (currently commented out)
-    """
-    with open("data/result/mileage.json", "r") as f:
-        data = json.load(f)
-    df = pd.DataFrame(data)
-    st.write("Rekord powiązany ze zdjęciem:")
-    record = df[df["Filename"] == path]
-    st.write(record)
-    # st.write("Wszystkie rekordy w pliku:")
-    # st.dataframe(df)
