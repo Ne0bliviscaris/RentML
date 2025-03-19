ad5e528cfcddfcb2629ef8f5d8e7d756198386b0
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-03-19 18:25:58 +0100

    proper chart using new json file

--- pages/2_New_Chart.py
+++ pages/2_New_Chart.py
@@ -1,16 +1,53 @@
+import altair as alt
 import pandas as pd
 import streamlit as st
 
 from modules.settings import JSON_FILE
 
+st.set_page_config(layout="wide")
 
-def load_json():
-    """Load JSON file and return it as a DataFrame."""
+
+def open_json(file):
     try:
-        return pd.read_json(JSON_FILE)
-    except (FileNotFoundError, ValueError):
-        return pd.DataFrame()
+        json_data = pd.read_json(file)
+    except:
+        json_data = pd.DataFrame()
+    return json_data
 
 
-new_json = load_json()
-st.altair_chart(new_json) if not new_json.empty else st.write("No data to display.")
+def show_chart():
+    """Display chart with car mileage data."""
+    json = open_json(JSON_FILE)
+    if json.empty:
+        st.warning("No data to display")
+        return
+    json["Date"] = pd.to_datetime(json["Date"]).dt.strftime("%Y-%m-%d")
+    json["Time"] = pd.to_datetime(json["Time"]).dt.strftime("%H:%M:%S")
+    json = json.sort_values("Date")
+
+    min_y = json["Mileage"].min()
+    max_y = json["Mileage"].max()
+    chart_scale = [min_y, max_y]
+
+    points = (
+        alt.Chart(json)
+        .mark_point()
+        .encode(x="Date:T", y=alt.Y("Mileage:Q", scale=alt.Scale(domain=chart_scale)), color="Car:N")
+    )
+
+    chart = (
+        alt.Chart(json)
+        .mark_line()
+        .encode(x="Date:T", y=alt.Y("Mileage:Q", scale=alt.Scale(domain=chart_scale)), color="Car:N")
+    )
+
+    chart = chart + points
+    st.altair_chart(chart.properties(width=800, height=400), use_container_width=True)
+
+
+def main():
+    st.title("New Chart")
+    show_chart()
+
+
+main()
