5d1324247de10a784d2b7ce50ae29a8acd0caf16
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-03-21 16:35:18 +0100

    use new, unified chart

--- pages/2_New_Chart.py
+++ pages/2_New_Chart.py
@@ -1,37 +1,10 @@
-import altair as alt
-import pandas as pd
 import streamlit as st
 
+from modules.charts import show_chart
 from modules.data_processing import open_json_as_df
 from modules.settings import JSON_FILE
 
 st.set_page_config(layout="wide")
-
-
-def show_chart(json):
-    """Display chart with car mileage data."""
-    json["Date"] = pd.to_datetime(json["Date"]).dt.strftime("%Y-%m-%d")
-    json["Time"] = pd.to_datetime(json["Time"]).dt.strftime("%H:%M:%S")
-    json = json.sort_values("Date")
-
-    min_y = json["Mileage"].min()
-    max_y = json["Mileage"].max()
-    chart_scale = [min_y, max_y]
-
-    points = (
-        alt.Chart(json)
-        .mark_point()
-        .encode(x="Date:T", y=alt.Y("Mileage:Q", scale=alt.Scale(domain=chart_scale)), color="Car:N")
-    )
-
-    chart = (
-        alt.Chart(json)
-        .mark_line()
-        .encode(x="Date:T", y=alt.Y("Mileage:Q", scale=alt.Scale(domain=chart_scale)), color="Car:N")
-    )
-
-    chart = chart + points
-    st.altair_chart(chart.properties(width=800, height=400), use_container_width=True)
 
 
 def main():
@@ -40,7 +13,8 @@ def main():
     if json.empty:
         st.warning("No data to display")
         return
-    show_chart(json)
+    chart = show_chart(json)
+    st.altair_chart(chart, use_container_width=True)
 
 
 main()
