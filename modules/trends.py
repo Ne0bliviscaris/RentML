b20b7ae224e9c652d5cc9c082453ccb708586332
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-03-25 16:26:48 +0100

    small bugfix

--- modules/trends.py
+++ modules/trends.py
@@ -4,12 +4,11 @@ from sklearn.neighbors import KNeighborsClassifier
 
 def predict_car(mileage, date, df, car_type=None):
     """Predict car model based on mileage and date"""
-    # Filter data by car_type if provided
     if car_type:
         df = df[df["Car type"] == car_type]
 
-    df_features = df.copy()
-    df_features["date_ordinal"] = df_features["Date"].map(pd.Timestamp.toordinal)
+    df = df.copy()
+    df["date_ordinal"] = df["Date"].map(pd.Timestamp.toordinal)
 
     model = KNeighborsClassifier(n_neighbors=3)
     x_train = df[["date_ordinal", "Mileage"]]
