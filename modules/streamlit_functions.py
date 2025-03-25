5da3e91e52efd36cb2fb79cf322ee4ebee512882
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-03-25 16:42:51 +0100

    feat: add car prediction functionality and improve car selection logic refactor: separated save and print protocol buttons as functions bugfix: protocol generator

--- modules/streamlit_functions.py
+++ modules/streamlit_functions.py
@@ -4,8 +4,9 @@ import pandas as pd
 import streamlit as st
 
 from modules.cars import Car
-from modules.data_processing import append_to_json
+from modules.data_processing import append_to_json, open_json_as_df
 from modules.docs_generator import generate_handover_protocol
+from modules.trends import predict_car
 
 
 def uploader():
@@ -15,35 +16,53 @@ def uploader():
     return st.file_uploader("Dodaj zdjęcie", type=["jpg", "png", "jpeg", "gif"])
 
 
+def car_form_prediction(mileage, date, car_type=None):
+    """Pre-fill car form with predicted car type."""
+    if mileage is None or date is None:
+        return None
+    cars_data = open_json_as_df()
+    return predict_car(mileage, date, cars_data, car_type)
+
+
 def confirmation_form(data=None):
     """Display editable form with pre-filled mileage and car type."""
-    mileage, car, date, time = data if data else (None, None, None, None)
+    mileage, car_type, date, time = data if data else (None, None, None, None)
 
     with st.form("Potwierdzenie danych", clear_on_submit=True, border=0):
-        # Edytowalny przebieg
         mileage = mileage_field(mileage)
-        car = car_selector(car)
+        predicted_car = car_form_prediction(mileage, date, car_type)
+        car = car_selector(car_type, predicted_car)
         date = date_field(date)
         time = time_field(time)
         notes = notes_field()
 
         col1, col2 = st.columns(2)
         with col1:
-            submitted = st.form_submit_button("Zapisz")
-            if submitted:
-                success = append_to_json(file_path=None, mileage=mileage, car=car, date=date, time=time, note=notes)
-                if success:
-                    st.success("Zapisano dane")
-                    st.session_state.form_submitted = True
-                else:
-                    st.warning("Dane istnieją już w bazie. Zaktualizowano notatkę.")
+            save_button(mileage, car.name, date, time, notes)
 
         with col2:
-            handover = st.form_submit_button("Print Handover Protocol")
-            if handover:
-                protocol = generate_handover_protocol(mileage=mileage, car=car, date=date, time=time, note=notes)
-                open_file(protocol)
-                st.success("Protokół zwrotu wygenerowany")
+            print_protocol_button(mileage, car, date, time, notes)
+
+
+def save_button(mileage, car_type, date, time, notes):
+    """Display save button."""
+    submitted = st.form_submit_button("Zapisz")
+    if submitted:
+        success = append_to_json(file_path=None, mileage=mileage, car=car_type, date=date, time=time, note=notes)
+        if success:
+            st.success("Zapisano dane")
+            st.session_state.form_submitted = True
+        else:
+            st.warning("Dane istnieją już w bazie. Zaktualizowano notatkę.")
+
+
+def print_protocol_button(mileage, car_type, date, time, notes):
+    """Display print handover protocol button."""
+    handover = st.form_submit_button("Print Handover Protocol")
+    if handover:
+        protocol = generate_handover_protocol(mileage=mileage, car=car_type, date=date, time=time, note=notes)
+        open_file(protocol)
+        st.success("Protokół zwrotu wygenerowany")
 
 
 def open_file(file_path):
@@ -76,20 +95,27 @@ def notes_field():
     return st.text_area("Notatki", value="")
 
 
-def car_selector(car_type):
+def first_car_of_type(car_type, all_cars):
+    """Return first car of given type."""
+    if car_type == "Osobowy":
+        return next((i for i, car in enumerate(all_cars) if "Osobowy" in car.car_type), 0)
+    elif car_type == "Dostawczy":
+        return next((i for i, car in enumerate(all_cars) if "Dostawczy" in car.car_type), 0)
+    else:
+        return 0
+
+
+def car_selector(predicted_car_type, predicted_car=None):
     """Display car type selector with default selection based on input value."""
     all_cars = Car.get_all_cars()
     cars_list = [car.name for car in all_cars]
 
-    if car_type == "Osobowy":
-        default_index = next((i for i, car in enumerate(all_cars) if "Osobowy" in car.car_type), 0)
-    elif car_type == "Dostawczy":
-        default_index = next((i for i, car in enumerate(all_cars) if "Dostawczy" in car.car_type), 0)
+    if predicted_car:
+        default_index = next((i for i, car in enumerate(all_cars) if predicted_car in car.name), 0)
     else:
-        default_index = 0
+        default_index = first_car_of_type(predicted_car_type, all_cars)
 
     selected_name = st.selectbox("Samochód", options=cars_list, index=default_index)
 
-    # Return the full Car object
     selected_car = next((car for car in all_cars if car.name == selected_name), all_cars[0])
     return selected_car
