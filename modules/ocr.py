691082289ec14ef8eceac12834d8cd832f6718fc
Ne0bliviscaris <133912560+Ne0bliviscaris@users.noreply.github.com>
2025-07-12 17:08:25 +0200

    refactor: simplify mileage OCR function - removed unused imports and functions - streamlined OCR process for 6-digit extraction

--- modules/ocr.py
+++ modules/ocr.py
@@ -1,38 +1,15 @@
-# v1.01 - Mileage extraction using EasyOCR
 import re
 
 import easyocr
-import numpy as np
-from PIL import Image, ImageOps
 
 
 def mileage_ocr(img):
-    """
-    Performs Optical Character Recognition (OCR) on an image to extract 6-digit numbers, typically representing mileage.
-    EasyOCR uses np.array as input.
-    """
-    img_array = load_image_as_array(img)
+    """Return first 6-digit number from OCR or None."""
+    ocr = easyocr.Reader(["en"], gpu=True)
+    img_bytes = img.read()
+    ocr_result = ocr.readtext(img_bytes, allowlist="0123456789")
 
-    # Create a reader to perform OCR
-    reader = easyocr.Reader(["en"], gpu=True)
+    SIX_DIGITS = r"\b\d{6}\b"
+    six_digit_numbers = re.findall(SIX_DIGITS, str(ocr_result))
 
-    # Read all text from image
-    result = reader.readtext(img_array, allowlist="0123456789")
-
-    # Find all 6-digit numbers
-    six_digit_numbers = re.findall(r"\b\d{6}\b", str(result))
-
-    # If no 6-digit number found, return empty variable
-    if not six_digit_numbers:
-
-        return None
-
-    # Return the list of 6-digit numbers as integers
-    return list(map(int, six_digit_numbers))
-
-
-def load_image_as_array(uploaded_file):
-    """Load an image from a file, and return it as a NumPy array."""
-    img = Image.open(uploaded_file)
-    img = ImageOps.exif_transpose(img)
-    return np.array(img)
+    return six_digit_numbers[0] if six_digit_numbers else None
